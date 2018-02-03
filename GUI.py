7a46cbdf8576c30ca6d7d9be8ba7f22f61eb057b
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2018-02-03 10:31:57 -0800

    GUI

--- GUI.py
+++ GUI.py
@@ -3,14 +3,18 @@ import tkinter as tk
 root = tk.Tk();
 root.attributes('-fullscreen', True)
 
-frame = tk.Frame(root)
-frame.pack()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.createWidgets()
 
-button = tk.Button(frame,
-                   text="QUIT",
-                   fg="red",
-                   command=quit)
-button.pack(side=tk.LEFT)
-button.place(y = 4)
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit', fg = "red",
+            command=self.quit)
+        self.quitButton.pack()
+        self.quitButton.grid()
 
-root.mainloop()
+app = Application()
+app.master.title('Sample application')
+app.mainloop()
