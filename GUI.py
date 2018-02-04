7dc0d9008a9991baef5c23e915799bc6554e13f7
Kelvin Chan <33172485+MyAngelNeptune@users.noreply.github.com>
2018-02-03 19:36:32 -0800

    GUI

--- GUI.py
+++ GUI.py
@@ -23,25 +23,25 @@ class Application(tk.Frame):
         canvas = tk.Canvas(root, width=1920, height=800)
 
         canvas.grid()
-        # canvas.create_text(1070, 50,"OSU UCISD")
+        canvas.create_text(1145, 50,text = "OSU UCISD", fill = "blue" , font="Times 35  bold")
 
         tk_img = ImageTk.PhotoImage(file=FILENAME)
+        title_img = ImageTk.PhotoImage(file="title.png")
+        osu_img = ImageTk.PhotoImage(file = "title2.png");
+
         canvas.create_image(500, 340, image=tk_img)
+        canvas.create_image(480, 100, image = title_img)
+        canvas.create_image(1150, 50, image = osu_img)
+
         fileInputButton = tk.Button(root, text= "Enter file", fg = "black", background = 'white', width = 20, height = 5,
-            command = self.load_file, activebackground="#33B5E5")
+            command = self.load_file, activebackground="#33B5E5", font="Times 10  bold")
         fileWindow = canvas.create_window(1070, 200, anchor='nw', window=fileInputButton)
-        quit_button = tk.Button(root, text="Quit", command=self.quit, anchor='w',
-                                width=20, activebackground="#33B5E5")
+        quit_button = tk.Button(root, text="Quit", fg = "black" , command=quit, background = 'white',
+                                width=20, height = 3,  activebackground="#33B5E5",font="Times 10  bold")
         quit_button_window = canvas.create_window(1070, 370, anchor='nw', window=quit_button)
 
         root.mainloop()
 
-        # self.fileInputButton.grid(row = 0, column = 1, pady = 200, ipadx = 50, ipady = 20)
-        # self.grid(row = 1, column = 1, padx = 1100, pady = 100)
-        # self.grid(row = 2, ipadx = 0, ipady = 100)
-        # self.quitButton = tk.Button(self, text='Quit', fg = "red",
-        #     command=self.quit)
-        # self.quitButton.grid(row = 3, column = 1, ipadx = 55, ipady = 20)
 
     def load_file(self):
         filename = filedialog.askopenfilename(filetypes = (("Template files", "*.txt")
